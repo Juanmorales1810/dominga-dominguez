3a654caa0fb89faa1ee44bf0cb7d704f03fc9e8c
Juanmorales1810 <juan1810.jm@gmail.com>
2025-02-14 10:40:36 -0300

    Feat: Añadir integración de sitemap y configurar la URL del sitio en la configuración de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://www.domingadominguez.cl",
     integrations: [
         tailwind({
             applyBaseStyles: false,
         }),
         ,
         react(),
+        sitemap(),
     ],
 });
